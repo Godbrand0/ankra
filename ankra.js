d2fc1e34eb93c6847960aa7807d35b6dc6a89f39
eregha thompson <thompsoneregha005@gmail.com>
2023-12-11 05:42:17 -0800

    first commit

--- ankra.js
+++ ankra.js
@@ -25,7 +25,7 @@ burger.addEventListener("click", function(){
 if (nav.style.display =='block') {
     nav.style.display = 'none';
 } else {
-    nav.style.display = 'block';
+    nav.style.display = '';
 }
 })
 
